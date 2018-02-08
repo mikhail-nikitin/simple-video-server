d38953315634f8bfe9cba9620f7f03730b5cc170
Mikhail Nikitin <mikhail.nikitin@ispringsolutions.com>
2018-02-09 01:48:02 +0300

    Get videos from repository in request handlers

--- handlers/get_video_info.go
+++ handlers/get_video_info.go
@@ -1,14 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
-func getVideoInfo(w http.ResponseWriter, _ *http.Request) {
-	videoDetails := Video{
-		Id:        "d290f1ee-6c54-4b01-90e6-d701748f0851",
-		Name:      "Black Retrospective Woman",
-		Duration:  15,
-		Thumbnail: "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-		Url:       "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4",
+func getVideoInfo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	videoId := vars["id"]
+	if video := getVideoById(videoId); video != nil {
+		renderJson(w, video)
+	} else {
+		http.NotFound(w, r)
 	}
-	renderJson(w, videoDetails)
 }
--- handlers/router.go
+++ handlers/router.go
@@ -10,7 +10,7 @@ func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/list", listVideos).Methods(http.MethodGet)
-	s.HandleFunc("/video/d290f1ee-6c54-4b01-90e6-d701748f0851", getVideoInfo).Methods(http.MethodGet)
+	s.HandleFunc("/video/{id}", getVideoInfo).Methods(http.MethodGet)
 	return logMiddleware(s)
 }
 
